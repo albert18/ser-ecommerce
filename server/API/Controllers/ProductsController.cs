699f52ed01654433fd2a88bf4812dfff71090fe1
albert18 <erertech@gmail.com>
2023-03-11 14:15:10 +0300

    update

--- server/API/Controllers/ProductsController.cs
+++ server/API/Controllers/ProductsController.cs
@@ -3,8 +3,20 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class ProductsController : ControllerBase
-    {
-        
+    {   
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "this will be a list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProducts(int id)
+        {
+            return "this will be a products";
+        }
+
     }
 }
