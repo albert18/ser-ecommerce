ce9e9909b6c15225602b6fa658c1ed19f3130ce4
albert18 <erertech@gmail.com>
2023-03-11 14:27:10 +0300

    update

--- server/API/Entities/Products.cs
+++ server/API/Entities/Products.cs
@@ -2,6 +2,7 @@ namespace API.Entities
 {
     public class Products
     {
-        public int Id { get; set; }       
+        public int Id { get; set; }
+        public string Name { get; set; }       
     }
 }
